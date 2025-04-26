c971103a2ed2318451de0d23d56d64bd3bdc3213
Alquince <alejandro.torres.tapia07@gmail.com>
2025-04-26 10:41:20 -0500

    Create PaginaWeb.py

---
+++ PaginaWeb.py
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dependecians Tecnológicas</title>
+    <link rel="stylesheet" href="Style.css">
+</head>
+<body>
+    <h1>¿Qué son las Dependecians Tecnológicas?</h1>
+    <p>Es un tipo de adicción en la que una persona no puede prescindir del uso de determinadas tecnologías y dispositivos. 
+        Esta adicción puede llevar a que uno pierda por completo la capacidad de funcionar sin estas tecnologías... Lo que puede tener un grave impacto negativo en su calidad de vida.</p>
+    <h2>Una lista de las dependencias tecnológicas más comunes:</h2>
+             <br>
+                   <li>Juegos y entretenimiento en línea: Los juegos y el ocio en línea pueden crear adicción y llevar a la gente a pasar demasiado tiempo en el mundo virtual en lugar de en la vida real.</li>
+                   <br> </br>
+                   <li>Tecnologías de Inteligencia Artificial: las personas pueden volverse dependientes del uso de tecnologías de inteligencia artificial, como los asistentes de voz, lo que puede provocar la degradación de las capacidades de comunicación y toma de decisiones.</li>
+             </ul>
+             <br></br>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsxOhykpxK3EyMtrfrdy89K1ozT3lqwpAJZg&s" alt="image">
+</body>
+</html>
+
+<iframe src="https://www.meteoblue.com/en/weather/widget/daily/ip_romania_675514?geoloc=fixed&days=7&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&coloured=monochrome&pictoicon=1&maxtemperature=1&mintemperature=1&windspeed=1&windgust=0&winddirection=1&uv=0&humidity=0&precipitation=1&precipitationprobability=1&spot=1&pressure=0&layout=dark"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 378px; height: 420px"></iframe>
